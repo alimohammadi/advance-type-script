da5245d23d6bdd1083b415107ef1f150e421b9c4
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-08-28 12:51:49 +0330

    Improve/ solve tuple challange 5

---
+++ src/4-Array-and-Tuples/5-challange.ts
@@ -0,0 +1,14 @@
+namespace nonEmptyArray {
+  type NonEmptyArray<T> = [T, ...T[]];
+
+  function sendMail(addresses: NonEmptyArray<string>) {
+    /* ... */
+  }
+
+  sendMail(["543 4th Ave"]); // ✅
+  sendMail(["34 vedi AStreenName", "568822 London"]); // ✅
+  
+  // @ts-expect-error
+  sendMail([]);
+  //       ^ ❌ `[]` isn't assignable to `NonEmptyArray<string>`
+}
