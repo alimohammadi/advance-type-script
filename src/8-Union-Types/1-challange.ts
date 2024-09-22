a5f6b77e2f8fcf11bef8ce1cdecdef4dee2d5608
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-09-22 16:39:38 +0330

    Solve union types challange 1

---
+++ src/8-Union-Types/1-challange.ts
@@ -0,0 +1,29 @@
+import { Equal, Expect } from "..";
+
+namespace getColor {
+  type LogStatus = "error" | "warning" | "info";
+
+  // type GetColor<Status extends LogStatus> = Status extends "error"
+  //   ? "red"
+  //   : Status extends "warning"
+  //   ? "orange"
+  //   : "blue";
+
+  type GetColor<Status extends LogStatus> = {
+    error: "red";
+    warning: "orange";
+    info: "blue";
+  }[Status];
+
+  type res1 = GetColor<"error">;
+  type test1 = Expect<Equal<res1, "red">>;
+
+  type res2 = GetColor<"error" | "warning">;
+  type test2 = Expect<Equal<res2, "red" | "orange">>;
+
+  type res3 = GetColor<"warning" | "info">;
+  type test3 = Expect<Equal<res3, "orange" | "blue">>;
+
+  type res4 = GetColor<"error" | "warning" | "info">;
+  type test4 = Expect<Equal<res4, "red" | "orange" | "blue">>;
+}
