3ba8c1e6ae8a506ff83dd75d3b60d7eb6ab76cb2
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-09-22 16:52:44 +0330

    Solve union types challange 4

---
+++ src/8-Union-Types/4-challange.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from "..";
+
+/**
+ * Note: The list of "falsy" values is
+ * false, null, 0, "", and undefined.
+ */
+namespace compact {
+  type Falsy = false | null | 0 | undefined | "";
+
+  declare function compact(list: Item[]): Exclude<Item, Falsy>[];
+
+  let res1 = compact([1, 2, null, 3, undefined, 4]);
+  type test1 = Expect<Equal<typeof res1, number[]>>;
+
+  let res2 = compact([undefined, 0 as const, "a", "", "b", "c", "d"]);
+  type test2 = Expect<Equal<typeof res2, string[]>>;
+
+  let res3 = compact([...([1, undefined, true, "", "hello", null] as const)]);
+  type test3 = Expect<Equal<typeof res3, (1 | true | "hello")[]>>;
+}
